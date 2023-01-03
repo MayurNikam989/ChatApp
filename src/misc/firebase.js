b20fec0015d9f5cb70fe3ae6788c4a05b47a3504
MayurNikam989 <mayurn989@gmail.com>
2023-01-03 17:47:18 +0530

    added signin method

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,8 +1,12 @@
 import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
 
 const config = {
   apiKey: "AIzaSyC810ytH9ssd4azUQU0lqwD8O78pjQjV1g",
   authDomain: "chat-web-app-34e5d.firebaseapp.com",
+  databaseURL:
+    "https://chat-web-app-34e5d-default-rtdb.asia-southeast1.firebasedatabase.app/",
   projectId: "chat-web-app-34e5d",
   storageBucket: "chat-web-app-34e5d.appspot.com",
   messagingSenderId: "894622959694",
@@ -10,3 +14,6 @@ const config = {
 };
 
 const app = firebase.initializeApp(config);
+
+export const auth = app.auth();
+export const database = app.database();
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,7 +1,71 @@
 import React from "react";
+import firebase from "firebase";
+import { Alert, Button, Col, Container, Grid, Icon, Panel, Row } from "rsuite";
+import { auth, database } from "../misc/firebase";
 
 const SignIn = () => {
-  return <div>SignIn</div>;
+  const SignInWithProvider = async (provider) => {
+    try {
+      //signinwithpop returns promises which contains object which we can destuctured directly.
+      const { additionalUserInfo, user } = await auth.signInWithPopup(provider);
+
+      //we can see promises(result) i.e, total objects on console
+      //   console.log("result", result);
+
+      if (additionalUserInfo.isNewUser) {
+        await database.ref(`/profiles/${user.uid}`).set({
+          name: user.displayName,
+          createdAt: firebase.database.ServerValue.TIMESTAMP,
+        });
+      }
+
+      Alert.success("Signed In SuccsFully", 4000);
+    } catch (error) {
+      Alert.error(error.message, 4000);
+    }
+  };
+
+  const onFacebookSignIn = () => {
+    SignInWithProvider(new firebase.auth.FacebookAuthProvider());
+  };
+  const onGoogleSignIn = () => {
+    SignInWithProvider(new firebase.auth.GoogleAuthProvider());
+  };
+
+  return (
+    <Container>
+      <Grid className="mt-page">
+        <Row>
+          <Col xs={24} md={12} mdOffset={6}>
+            <Panel>
+              <div className="text-center">
+                <h2>Welcome To Chat App</h2>
+                <p>A Progressive Chat Application</p>
+              </div>
+              <div className="mt-3">
+                <Button
+                  block={true}
+                  appearance="ghost"
+                  color="blue"
+                  onClick={onFacebookSignIn}
+                >
+                  <Icon icon="facebook"></Icon> Continue With Facebook
+                </Button>
+                <Button
+                  block={true}
+                  appearance="ghost"
+                  color="green"
+                  onClick={onGoogleSignIn}
+                >
+                  <Icon icon="google"></Icon> Continue With Google
+                </Button>
+              </div>
+            </Panel>
+          </Col>
+        </Row>
+      </Grid>
+    </Container>
+  );
 };
 
 export default SignIn;
